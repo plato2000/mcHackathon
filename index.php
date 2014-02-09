e62c1e277b04b3411ec07fd5d0aef566f440f920
Aman Jaiman <amanjaiman>
2014-02-08 19:00:51 -0500

    Changed all the things

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <!-- saved from url=(0043)http://getbootstrap.com/examples/dashboard/ -->
-<!DOCTYPE html PUBliC "" "">
 <html lang="en">
 
 <head>
   <meta content="IE=11.0000" http-equiv="X-UA-Compatible">
-
+	<script src="js/jquery-2.1.0.js"></script>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="">
   <meta name="author" content="">
   <link href="../../assets/ico/favicon.ico" rel="shortcut icon">
+  <style type="text/css">
+      html { height: 100% }
+      body { height: 100%; margin: 0; padding: 0 }
+      #map-canvas { height: 660px; width: 100%;}
+	  $('.dropdown-toggle').dropdown()
+    </style>
   <title>Dashboard Template for Bootstrap</title>
   <!-- Bootstrap core CSS -->
   <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -20,7 +25,25 @@
   <!-- Just for debugging purposes. Don't actually copy this line! -->
   <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
   <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBJC6lElxXIUzG55JY73IskWiFzRdRf9LI&sensor=true"></script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			console.log("Reached ready");
+			var map;
+			function initialize() {
+			  var mapOptions = {
+				zoom: 7,
+				center: new google.maps.LatLng(39, -76.7),
+				<!--Later make the coordinates depend on where the user is-->
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			  };
+			  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+			}
+			google.maps.event.addDomListener(window, 'load', initialize);
+		});
 
+	</script>
+	<script src="js/script.js" type="text-javascript"></script>
   <meta name="GENERATOR" content="MShtml 11.00.9600.16476">
 </head>
 
@@ -34,213 +57,53 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="http://getbootstrap.com/examples/dashboard/#">Project name</a>
+        <a class="navbar-brand" href="#">Demographics</a>
+	<!--<p>Made by [Group Name Here]</p>-->
       </div>
       <div class="navbar-collapse collapse">
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="http://getbootstrap.com/examples/dashboard/#">Dashboard</a>
+        <ul class="nav navbar-nav navbar-right"> <!--If user is not signed in, show the various way to sign in in a drop-down menu-->
+          <li><a href="#">Dashboard</a>
           </li>
-          <li><a href="http://getbootstrap.com/examples/dashboard/#">Settings</a>
+          <li><a href="#">Profile</a>
           </li>
-          <li><a href="http://getbootstrap.com/examples/dashboard/#">Profile</a>
+          <li><a href="#">Help</a>
           </li>
-          <li><a href="http://getbootstrap.com/examples/dashboard/#">Help</a>
-          </li>
+		  <li><a href="#"><img src="#" /></a> <!--The link for this a href will be the same for the profile-->
+		  </li>
         </ul>
-        <form class="navbar-form navbar-right">
-          <input class="form-control" type="text" placeholder="Search..." value="">
-        </form>
       </div>
     </div>
   </div>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-sm-3 col-md-2 sidebar">
-        <ul class="nav nav-sidebar">
-          <li class="active"><a href="http://getbootstrap.com/examples/dashboard/#">Overview</a>
-          </li>
-          <li><a href="/#">Reports</a>
-          </li>
-          <li><a href="/#">Analytics</a>
-          </li>
-          <li><a href="/#">Export</a>
-          </li>
-        </ul>
-        <ul class="nav nav-sidebar">
-          <li><a href="/#">Nav item</a>
-          </li>
-          <li><a href="/#">Nav item 
-	  again</a>
-          </li>
-          <li><a href="/#">One more 
-	nav</a>
-          </li>
-          <li><a href="/#">Another nav 
-	  item</a>
-          </li>
-          <li><a href="/#">More 
-	  navigation</a>
-          </li>
-        </ul>
-        <ul class="nav nav-sidebar">
-          <li><a href="/#">Nav item 
-	  again</a>
-          </li>
-          <li><a href="/#">One more 
-	nav</a>
-          </li>
-          <li><a href="/#">Another nav 
-	  item</a>
-          </li>
-        </ul>
+      <div class="col-md-4 sidebar">
+	  <!--<form class="navbar-form navbar-right">-->
+		<div style="width: 400px;">
+          <input class="form-control" type="text" placeholder="Search..." value="">
+		  <br />
+		</div>
+        <!--</form>-->
+        <div style = "width: 100%;">
+		<div class="col-md-9" role="main">
+		<div class="btn-group">
+		<button type="button" class= "btn btn-default dropdown-toggle" data-toggle="dropdown">Gender <span class="caret"></span></button>
+              <ul class="dropdown-menu" role="menu" aria-labelledby="drop1">
+                <li role="presentation"><a tabindex="-1" role="menuitem" id="male"><!-- href="http://twitter.com/fat"-->Male</a></li>
+				<!--<li class="divider" role="presentation"></li>-->
+                <li role="presentation"><a tabindex="-1" role="menuitem" id="female"><!-- href="http://twitter.com/fat"-->Female</a></li>
+                <!--<li role="presentation"><a tabindex="-1" role="menuitem"<!-- href="http://twitter.com/fat">Business</a></li>-->
+              </ul>
+		</div>
+		</div>	 
+		</div>
       </div>
       <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-        <H1 class="page-header">Dashboard</H1>
-        <div class="row placeholders">
-          <div class="col-xs-6 col-sm-3 placeholder">
-            <IMG class="img-responsive" alt="Generic placeholder thumbnail" data-src="holder.js/200x200/auto/sky">
-            <h4>Label</h4><span class="text-muted">Something else</span> 
-          </div>
-          <div class="col-xs-6 col-sm-3 placeholder">
-            <IMG class="img-responsive" alt="Generic placeholder thumbnail" data-src="holder.js/200x200/auto/vine">
-            <h4>Label</h4><span class="text-muted">Something else</span> 
-          </div>
-          <div class="col-xs-6 col-sm-3 placeholder">
-            <IMG class="img-responsive" alt="Generic placeholder thumbnail" data-src="holder.js/200x200/auto/sky">
-            <h4>Label</h4><span class="text-muted">Something else</span> 
-          </div>
-          <div class="col-xs-6 col-sm-3 placeholder">
-            <IMG class="img-responsive" alt="Generic placeholder thumbnail" data-src="holder.js/200x200/auto/vine">
-            <h4>Label</h4><span class="text-muted">Something else</span> 
+        <div id="map-canvas"/>
           </div>
         </div>
         <H2 class="sub-header">Section title</H2>
         <div class="table-responsive">
-          <table class="table table-striped">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Header</th>
-                <th>Header</th>
-                <th>Header</th>
-                <th>Header</th>
-              </tr>
-            </thead>
-            <Tbody>
-              <tr>
-                <td>1,001</td>
-                <td>Lorem</td>
-                <td>ipsum</td>
-                <td>dolor</td>
-                <td>sit</td>
-              </tr>
-              <tr>
-                <td>1,002</td>
-                <td>amet</td>
-                <td>consectetur</td>
-                <td>adipiscing</td>
-                <td>elit</td>
-              </tr>
-              <tr>
-                <td>1,003</td>
-                <td>Integer</td>
-                <td>nec</td>
-                <td>odio</td>
-                <td>Praesent</td>
-              </tr>
-              <tr>
-                <td>1,003</td>
-                <td>libero</td>
-                <td>Sed</td>
-                <td>cursus</td>
-                <td>ante</td>
-              </tr>
-              <tr>
-                <td>1,004</td>
-                <td>dapibus</td>
-                <td>diam</td>
-                <td>Sed</td>
-                <td>nisi</td>
-              </tr>
-              <tr>
-                <td>1,005</td>
-                <td>Nulla</td>
-                <td>quis</td>
-                <td>sem</td>
-                <td>at</td>
-              </tr>
-              <tr>
-                <td>1,006</td>
-                <td>nibh</td>
-                <td>elementum</td>
-                <td>imperdiet</td>
-                <td>Duis</td>
-              </tr>
-              <tr>
-                <td>1,007</td>
-                <td>sagittis</td>
-                <td>ipsum</td>
-                <td>Praesent</td>
-                <td>mauris</td>
-              </tr>
-              <tr>
-                <td>1,008</td>
-                <td>Fusce</td>
-                <td>nec</td>
-                <td>tellus</td>
-                <td>sed</td>
-              </tr>
-              <tr>
-                <td>1,009</td>
-                <td>augue</td>
-                <td>semper</td>
-                <td>porta</td>
-                <td>Mauris</td>
-              </tr>
-              <tr>
-                <td>1,010</td>
-                <td>massa</td>
-                <td>Vestibulum</td>
-                <td>lacinia</td>
-                <td>arcu</td>
-              </tr>
-              <tr>
-                <td>1,011</td>
-                <td>eget</td>
-                <td>nulla</td>
-                <td>Class</td>
-                <td>aptent</td>
-              </tr>
-              <tr>
-                <td>1,012</td>
-                <td>taciti</td>
-                <td>sociosqu</td>
-                <td>ad</td>
-                <td>litora</td>
-              </tr>
-              <tr>
-                <td>1,013</td>
-                <td>torquent</td>
-                <td>per</td>
-                <td>conubia</td>
-                <td>nostra</td>
-              </tr>
-              <tr>
-                <td>1,014</td>
-                <td>per</td>
-                <td>inceptos</td>
-                <td>himenaeos</td>
-                <td>Curabitur</td>
-              </tr>
-              <tr>
-                <td>1,015</td>
-                <td>sodales</td>
-                <td>ligula</td>
-                <td>in</td>
-                <td>libero</td>
-              </tr>
-            </Tbody>
-          </table>
+          
         </div>
       </div>
     </div>
@@ -248,11 +111,10 @@
   <!-- Bootstrap core JavaScript
 		================================================== -->
   <!-- Placed at the end of the document so the pages load faster -->
-  <script src="js/jquery-1.10.2.min.js"></script>
 
   <script src="js/bootstrap.min.js"></script>
 
-  <script src="js/docs.min.js"></script>
+  <!--<script src="js/docs.min.js"></script>-->
 </body>
 
 </html>
